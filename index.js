b23da5acf43ac821e1428ca67b3309801d077d15
Kon <knguyen6@uw.edu>
2015-03-10 23:09:11 -0700

    connected to my sql

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 var express = require('express'), 
-	mysql = require('mysql');
+	mysql = require('mysql'),
+	path = require('path');
 var app = express();
 
+/*these are to load local resources (any file in libclone after '/' such as pictures and main.css
+without them the pictures or css file.*/
+app.use(express.static(__dirname+'/'));
+app.use('/pictures', express.static(__dirname+'/'));
+/*http://codeforgeek.com/2015/01/render-html-file-expressjs/
+ Allow server to load html files.
+*/
 app.get('/', function(req, res) {
-	res.sendFile(__dirname+'/login.html');
-
-});
-app.get('/signuppage', function(req, res){
-	res.sendFile(__dirname+'/signuppage.html');
-});
-app.get('/dashboard', function(req, res){
-	res.sendFile(__dirname+'/dashboard.html');
+	res.sendFile(path.join(__dirname+'/login.html'));
 });
 
 var server = app.listen(8080, function(){
@@ -20,22 +21,23 @@ var server = app.listen(8080, function(){
 	console.log(host+ ' : '+port);
 });
 
-// var connection = mysql.createConnection({
-// 	host:'KonNguyen',
-// 	user:'root',
-// 	password:'password'
-// });
+var connection = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	password:'password',
+	database : 'nguyen_khanh_db'
+});
 
-// connection.connect();
+connection.connect();
 
-// connection.query('select * from bookauthor', function(err, rows, fields){
+connection.query('select * from bookauthor', function(err, rows, fields){
 
-// 	if(err) 
-// 		 console.log(err);
-	
-// 	console.log('connect successfully');
-// });
+	if(err) 
+		 console.log(err);
+	else
+	console.log('connect successfully');
+});
 
 
 
-// connection.end();
+connection.end();
