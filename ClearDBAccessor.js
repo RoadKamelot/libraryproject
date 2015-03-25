42f8386efb20c3b0f54728e047666f279432f108
bokuwa <knguyen6@uw.edu>
2015-03-25 00:21:18 -0700

    When a user registers, it updates both tables userinfo and useraccount in ClearDB

--- ClearDBAccessor.js
+++ ClearDBAccessor.js
@@ -23,24 +23,56 @@ function _findMatchingUsernameAndPassword(username, password, callback) {
 	            var result = _und.find(dbResult, function(row) {
 	                return row.username == username && row.pw == password;
 	            });
-                callback(result);
+                callback(null, result);
 	        }
 	    });
 	});
 }
 
-function _findMatchingUsernameOrEmail(username, email, callback) {
+function _registerAndValidateUser(post, callback) {
+	var username = post.Username,
+		email = post.Email,
+		useraccountPost = { Username : post.Username, Pw : post.Password };
+	delete post['Password'];
+
 	pool.getConnection(function(err, connection) {
 		checkSQLConnection(err, connection);
 	    connection.query('select Username, Email from userinfo', function(err, dbResult) {
-	        connection.release();
 	        if (err) {
-	            return err;
+	            return callback(err, null);
 	        } else {
-	            var result = _und.find(dbResult, function(row) {
+	            var searchUser = _und.find(dbResult, function(row) {
 	                return row.username == username || row.Email == email;
 	            });
-                callback(result);
+
+	            if (searchUser != null) {
+	            	console.log('It is really in here');
+	            	return callback(new Error('Duplicated User'), null);
+	            }
+
+	            connection.query('insert into userinfo set ?', post, function(err, dbResult, fields) {
+	            	//{"fieldCount":0,"affectedRows":1,"insertId":182,"serverStatus":2,"warningCount":0,"message":"","protocol41":true,"changedRows":0}
+
+	                if (err) {
+	                    return callback(err, null);
+	                } else {
+	                	useraccountPost.UserNo = dbResult.insertId;
+
+	                	connection.query('insert into useraccount set ?', useraccountPost, function(err, dbResult, fields) {
+			            	connection.release();
+			            	if (err) {
+			            		return callback(err, null);
+			            	} else {
+			            		return callback(null, useraccountPost);
+			            	}
+		            	});
+	                }
+	            });
+
+	            console.log('useraccountPost: ' + JSON.stringify(useraccountPost));
+
+	            //insert into useraccount (UserNo, Username, Pw) values(888, 'inno23', 'xxxx');
+	            
 	        }
 	    });
 	});
@@ -56,7 +88,6 @@ function _findMatchingISBN(isbn, callback) {
                 var result = _und.find(dbResult, function(row) {
                 	return row.ISBN == isbn; //isbn == req.headers.isbn
                 });
-                var template = '<div class="container searchbox-div"><div><strong>' + result.isbn + '</strong></div><div>' + result.author + '</div><div>' + result.title + '</div><div> ' + result.category + '</div></div>';
                 callback(result);
             }
 		});
@@ -76,5 +107,6 @@ function checkSQLConnection(err, connection) {
 
 module.exports = {
 	findMatchingUsernameAndPassword : _findMatchingUsernameAndPassword,
+	registerAndValidateUser : _registerAndValidateUser,
 	findMatchingISBN : _findMatchingISBN
 };
